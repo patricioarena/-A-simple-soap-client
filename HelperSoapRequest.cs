cf8fbd1211b20becb3d93b6b21fad939fedad275
Patricio Arena <patricio.e.arena@gmail.com>
2019-07-04 11:04:53 -0300

    Cliente soap simple funciona pero falta mejorarlo

---
+++ HelperSoapRequest.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CallSOAP
+{
+    public class HelperSoapRequest
+    {
+        private string path;
+        private string url;
+        public HelperSoapRequest(string path, string url){
+            this.path = path;
+            this.url = url;
+        }
+        public string InvokeService()
+        {
+            HttpWebRequest request = CreateSOAPWebRequest();
+            XmlDocument SOAPReqBody = new XmlDocument();
+
+            SOAPReqBody.Load(path);
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                SOAPReqBody.Save(stream);
+            }
+            using (WebResponse Serviceres = request.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(Serviceres.GetResponseStream()))
+                {
+                    var ServiceResult = rd.ReadToEnd();
+
+                    return this.JsonResponse(ServiceResult);
+                }
+            }
+        }
+
+        public HttpWebRequest CreateSOAPWebRequest()
+        {
+            HttpWebRequest Req = (HttpWebRequest)WebRequest.Create(url);
+            Req.ContentType = "application/soap+xml;charset=\"utf-8\"";
+            Req.Method = "POST";
+            return Req;
+        }
+        public string JsonResponse(string input)
+        {
+            using (var xReader = XmlReader.Create(new StringReader(input)))
+            {
+                xReader.MoveToContent();
+                xReader.Read();
+                XNode node = XNode.ReadFrom(xReader);
+                string jsonText = JsonConvert.SerializeXNode(node);
+                return jsonText;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using CallSOAP;
+using System;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace UsingSOAPRequest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            var path = @"C:\Users\parena\Desktop\xmlCorte\soap12.xml";
+            var url = @"https://tasatesting.scba.gov.ar/ws/service.asmx";
+
+            HelperSoapRequest helper = new HelperSoapRequest(path , url);
+            var response = helper.InvokeService();
+            
+            Console.WriteLine(response);
+
+
+        }
+
+    }
+}
