4efa69cd7f56104bf5abf05ef0c6f9530d0e70e7
Patricio Arena <patricio.e.arena@gmail.com>
2019-07-05 14:49:24 -0300

    Preparando para convertir esto en servicio.

---
+++ Models/BoletaPagoTasaJustica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallSOAP.Models
+{
+    public class BoletaPagoTasaJustica
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int TipoDocumento { get; set; }
+        public int NroDocumento { get; set; }
+        public decimal ImpuestoAbonar { get; set; }
+        public string Caratula { get; set; }
+        public decimal BaseImponible { get; set; }
+        public int idOrganismo { get; set; }
+        public string LetraReceptoria { get; set; }
+        public int NumeroReceptoria { get; set; }
+        public string SufijoReceptoria { get; set; }
+        public string CodigoBarras { get; set; }
+        public string DetalleError { get; set; }
+    }
+}
---
+++ Models/CodigoBarrasTasaJusticia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallSOAP.Models
+{
+    public class CodigoBarrasTasaJusticia
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string TipoDocumento { get; set; }
+        public string NroDocumento { get; set; }
+        public double ImpuestoAbonar { get; set; }
+        public string Caratula { get; set; }
+        public int nroJuicio { get; set; }
+        public double BaseImponible { get; set; }
+        public int idOrganismo { get; set; }
+        public string LetraReceptoria { get; set; }
+        public int NumeroReceptoria { get; set; }
+        public int SufijoReceptoria { get; set; }
+        public string DetalleError { get; set; }
+
+    }
+}
